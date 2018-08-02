102b1dfcdad6b61518a65d6fe2388d6dc5447b09
Rafael Prado <rps4@cin.ufpe.br>
2018-08-02 11:29:38 -0300

    Funcionando para numeros pequenos de primos

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,38 @@
 import { Component } from '@angular/core';
 
-let primeNums = [];
-
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
   title = 'primos';
-  numbers = primeNums;
+  numbers = [];
+
+  isPrime (num: number): Boolean {
+    var limit=Math.sqrt(num);
+    for ( var i = 2; i <= limit; i++ ) {
+        if ( num % i === 0 ) {
+            return false;
+        }
+    }
+    return true;
 	}
 
+  generatePrimeNumbers (): void {
+  	this.numbers = [{id: 2}];
+		for (var i = 3; i<100; i++){
+			if ( this.isPrime(i) ) {
+				this.numbers = [{id: i}].concat(this.numbers);
+				
+			}
+		}
+  }
+
+  trackByFn(index, item) {
+    return index;
+  }
 }
 
 
