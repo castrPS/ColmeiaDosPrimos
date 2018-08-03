b2ac8775abc7ccebbaeb8e11a9dd9313245de789
Rafael Prado <rps4@cin.ufpe.br>
2018-08-02 22:41:11 -0300

    Criação da página de informações e ajustes no roteamento

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,39 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule, Routes } from '@angular/router';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { InfoComponent } from './info.component';
+import { MainComponent } from './main.component';
+
+const appRoutes: Routes = [
+	{
+		path: 'info',
+		component: InfoComponent
+	},
+  {
+    path: '',
+    component: MainComponent
+  },
+  {
+    path: 'home',
+    component: MainComponent
+  }
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainComponent,
+    InfoComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(
+      appRoutes,
+      { enableTracing: true } // <-- debugging purposes only
+    )
+
   ],
   providers: [],
   bootstrap: [AppComponent]
