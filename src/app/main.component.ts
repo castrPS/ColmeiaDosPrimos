b74fb20d0580794e7b5f4b1b663872602b59566b
Rafael Prado <rps4@cin.ufpe.br>
2018-08-06 00:48:38 -0300

    Mudanca do algoritmo para adaptacao do crivo de eratostenes

--- src/app/main.component.ts
+++ src/app/main.component.ts
@@ -17,68 +17,63 @@ export class MainComponent{
   //verifica se o numero e primo analisando até a sua raiz se possui algum divisor
   isPrime (num: number): Boolean {
     var limit=Math.sqrt(num);
-    for ( var i = 2; i <= limit; i++ ) {
-        if ( num % i === 0 ) {
+   for (var i = 1; this.numbers[i] < limit; i++) {
+        if ( num % this.numbers[i] === 0 ) {
             return false;
         }
     }
     return true;
-	}
+  }
 
   //dado o ultimo numero gerado, gera o primo seguinte 
   generatePrimeNumber (num: number): number{
-		for (var i = num+1; i>0; i++){
-			if ( this.isPrime(i) ) {
-				return i;
-			}
-		}
+    for (var i = num+2; i>0; i++){
+      if ( this.isPrime(i) ) {
+        return i;
+      }
+    }
   }
   
   start (): void {
-  	this.started = true;
-  	this.paused = false;
-  	this.stopped=false;
-  	this.actual=2;
-  	this.numbers = [this.actual];
-    //chama a função a cada 0,5 segundos para gerar um novo primo
-  	var interval = setInterval(()=>{
-  		if(!this.stopped && !this.paused){
-  			this.actual= this.generatePrimeNumber(this.actual);
-  			this.numbers = [this.actual].concat(this.numbers);
-  		}
-  		else{
-  			clearInterval(interval);
-  		}
-  	},250);
+    this.started = true;
+    this.paused = false;
+    this.stopped=false;
+    this.actual=3;
+    this.numbers = [2,3];
+    //chama a função a cada 0,05 segundos para gerar um novo primo
+    var interval = setInterval(()=>{
+      if(!this.stopped && !this.paused){
+        this.actual= this.generatePrimeNumber(this.actual);
+        this.numbers = [this.actual].concat(this.numbers);
+      }
+      else{
+        clearInterval(interval);
+      }
+    },50);
   }
 
   pause(): void{
-  	this.paused = true;
+    this.paused = true;
   }
 
   resume(): void{
-  	this.paused = false;
-    //chama a função a cada 0,5 segundos para gerar um novo primo
-  	var interval = setInterval(()=>{
-  		if(!this.stopped && !this.paused){
-  			this.actual= this.generatePrimeNumber(this.actual);
-  			this.numbers = [this.actual].concat(this.numbers);
-  		}
-  		else{
-  			clearInterval(interval);
-  		}
-  	},250);
+    this.paused = false;
+    //chama a função a cada 0,05 segundos para gerar um novo primo
+    var interval = setInterval(()=>{
+      if(!this.stopped && !this.paused){
+        this.actual= this.generatePrimeNumber(this.actual);
+        this.numbers = [this.actual].concat(this.numbers);
+      }
+      else{
+        clearInterval(interval);
+      }
+    },50);
   }
 
   stop(): void{
-  	this.started=false;
-  	this.paused=false;
-  	this.stopped=true;
-  }
-
-  clear(): void{
-  	this.numbers= [];
-  	this.actual=2;
+    this.started=false;
+    this.paused=false;
+    this.stopped=true;
   }
 
   download(): void{
