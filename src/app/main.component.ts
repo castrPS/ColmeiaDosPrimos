05b7d76b2a9324ebd50eff12c3da251e4b445224
Rafael Prado <rps4@cin.ufpe.br>
2018-08-06 02:36:50 -0300

    Crivo de atkins feito, mas com o problema de ter que ser chamado diversas vezes

--- src/app/main.component.ts
+++ src/app/main.component.ts
@@ -8,70 +8,71 @@ import { Component } from '@angular/core';
 
 export class MainComponent{
   title = 'primos';
-  numbers = [];
-  sieve = [];
-  actual=0;
+  sieveNumber: number;
+  constant: number;
+  numbers=[];
+
   started = false;
   paused = false;
   stopped = false;
-  sieveNumber = 0;
-  constant = 100;
 
-  //verifica se o numero e primo analisando até a sua raiz se possui algum divisor
-  isPrime (num: number): Boolean {
-    var limit=Math.sqrt(num);
-   for (var i = 1; this.numbers[i] < limit; i++) {
-        if ( num % this.numbers[i] === 0 ) {
-            return false;
+  sieveOfAtkin(init: number,limit: number): boolean[]{
+   var results = [];
+   var sieve = new Array(limit+1);
+  
+   for(var i = 0; i < sieve.length; i++){
+    sieve[i] = 0;
+   }
+
+   var factor = Math.floor(Math.sqrt(limit))+1;
+   var n,i,j;
+  
+   for(i = 1; i < factor; i++){
+    for(j = 1; j < factor; j++){
+      n = 4 * (Math.pow(i,2)) + (Math.pow(j,2));
+      
+      if((n <= limit) && ((n % 12 === 1) || (n % 12 === 5))){
+        sieve[n] = sieve[n] ^ 1;
+      }
+      
+      n = 3 * (Math.pow(i,2)) + (Math.pow(j,2));
+      
+      if((n <= limit) && (n % 12 === 7)){
+        sieve[n] = sieve[n] ^ 1;
+      }
+      
+      if(i > j){
+        n = 3 * (Math.pow(i,2)) - (Math.pow(j,2));
+        
+        if((n <= limit) && (n % 12 === 11)){
+          sieve[n] = sieve[n] ^ 1;
         }
+      }
+     } 
+   }
+  
+  for(i = init; i < factor; i++){
+    if(sieve[i] === 1){
+      for(j = (Math.pow(i,2)); j < limit; j += Math.pow(i,2)){
+        sieve[j] = 0;
+      }
     }
-    return true;
   }
-
-  sieveOfAtkin(init: number,limit: number): void{
-   var limitSqrt = Math.sqrt(limit);
-   var n;
-
-   for (var x = 1; x <= limitSqrt; x++) {
-       var xx = x*x;
-       for (var y = 1; y <= limitSqrt; y++) {
-           var yy = y*y;
-           if (xx + yy >= limit) {
-             break;
-           }
-           // first quadratic using m = 12 and r in R1 = {r : 1, 5}
-           n = (4 * xx) + (yy);
-           if (n <= limit && (n % 12 == 1 || n % 12 == 5)) {
-               this.sieve[n] = !this.sieve[n];
-           }
-           // second quadratic using m = 12 and r in R2 = {r : 7}
-           n = (3 * xx) + (yy);
-           if (n <= limit && (n % 12 == 7)) {
-               this.sieve[n] = !this.sieve[n];
-           }
-           // third quadratic using m = 12 and r in R3 = {r : 11}
-           n = (3 * xx) - (yy);
-           if (x > y && n <= limit && (n % 12 == 11)) {
-               this.sieve[n] = !this.sieve[n];
-           }
-       }
-   }
-
-   // false each primes multiples
-   for (n = 5; n <= limitSqrt; n++) {
-       if (this.sieve[n]) {
-           x = n * n;
-           for (var i = x; i <= limit; i += x) {
-               this.sieve[i] = false;
-           }
-       }
-   }
+  
+  // If the number is prime, push to the results array
+  for(i = init; i < limit; i++){
+    if(sieve[i] ===1){
+      this.numbers= [i].concat(this.numbers);
+    }
+  }
+  
+  return results;
 }
 
   //dado o ultimo numero gerado, gera o primo seguinte 
   getPrimeNumber (num: number): number{
     for(var i = num+2; i > 0; i= i+2){
-        if (this.sieve[i]){
+        if (this.numbers[i]){
             return i;
         }
     }
@@ -81,18 +82,15 @@ export class MainComponent{
     this.started = true;
     this.paused = false;
     this.stopped=false;
-    this.actual=3;
-    this.numbers = [3,2];
-    this.sieve[2] = true;
-    this.sieve[3] = true;
+    this.numbers=[3,2];
+    this.sieveNumber = 0;
+    this.constant = 100;
 
     var interval1 = setInterval(()=>{
       if(!this.stopped && !this.paused){
         var newSieve = this.sieveNumber + this.constant;
         this.sieveOfAtkin(this.sieveNumber,newSieve);
         this.sieveNumber = newSieve;
-        this.actual= this.getPrimeNumber(this.actual);
-        this.numbers = [this.actual].concat(this.numbers);
       }
       else{
         clearInterval(interval1);
@@ -112,8 +110,6 @@ export class MainComponent{
         var newSieve = this.sieveNumber + this.constant;
         this.sieveOfAtkin(this.sieveNumber,newSieve);
         this.sieveNumber = newSieve;
-        this.actual= this.getPrimeNumber(this.actual);
-        this.numbers = [this.actual].concat(this.numbers);
       }
       else{
         clearInterval(interval1);
