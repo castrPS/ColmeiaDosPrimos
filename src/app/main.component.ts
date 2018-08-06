46f05cdb94f5d2f93ed15cfc7749cd9b853f0a41
Rafael Prado <rps4@cin.ufpe.br>
2018-08-06 08:24:19 -0300

    Ajustes no codigo, identacao e otimizacoes
    Retirada do reverse pipe não utilizado
    
    Ajustes

--- src/app/main.component.ts
+++ src/app/main.component.ts
@@ -16,68 +16,68 @@ export class MainComponent{
   paused = false;
   stopped = false;
 
-  sieveOfAtkin(init: number,limit: number): boolean[]{
-   var results = [];
-   var sieve = new Array(limit+1);
-  
-   for(var i = 0; i < sieve.length; i++){
-    sieve[i] = 0;
-   }
+  sieveOfAtkin(init: number,limit: number): void{
+    var results = [];
+    var sieve = new Array(limit+1);
 
-   var factor = Math.floor(Math.sqrt(limit))+1;
-   var n,i,j;
-  
-   for(i = 1; i < factor; i++){
-    for(j = 1; j < factor; j++){
-      n = 4 * (Math.pow(i,2)) + (Math.pow(j,2));
-      
-      if((n <= limit) && ((n % 12 === 1) || (n % 12 === 5))){
-        sieve[n] = sieve[n] ^ 1;
-      }
-      
-      n = 3 * (Math.pow(i,2)) + (Math.pow(j,2));
-      
-      if((n <= limit) && (n % 12 === 7)){
-        sieve[n] = sieve[n] ^ 1;
-      }
-      
-      if(i > j){
-        n = 3 * (Math.pow(i,2)) - (Math.pow(j,2));
-        
-        if((n <= limit) && (n % 12 === 11)){
-          sieve[n] = sieve[n] ^ 1;
+    for(var i = 0; i < sieve.length; i++){
+      sieve[i] = 0;
+    }
+
+    var factor = Math.floor(Math.sqrt(limit))+1;
+    var n;
+    
+    for(var i = 1; i < factor; i++){
+      for(var j = 1; j < factor; j++){
+
+        n = 4 * (Math.pow(i,2)) + (Math.pow(j,2));
+        if((n <= limit) && ((n % 12 === 1) || (n % 12 === 5))){
+          sieve[n] = !sieve[n];
+        }
+
+        n = 3 * (Math.pow(i,2)) + (Math.pow(j,2));
+        if((n <= limit) && (n % 12 === 7)){
+          sieve[n] = !sieve[n];
+        }
+
+        if(i > j){
+          n = 3 * (Math.pow(i,2)) - (Math.pow(j,2));
+          if((n <= limit) && (n % 12 === 11)){
+            sieve[n] = !sieve[n];
+          }
+        }
+      } 
+    }
+
+    for(i = 1; i < factor; i++){
+      if(sieve[i]){
+        for(j = (Math.pow(i,2)); j < limit; j += Math.pow(i,2)){
+          sieve[j] = false;
         }
       }
-     } 
-   }
-  
-  for(i = init; i < factor; i++){
-    if(sieve[i] === 1){
-      for(j = (Math.pow(i,2)); j < limit; j += Math.pow(i,2)){
-        sieve[j] = 0;
-      }
     }
-  }
-  
-  // If the number is prime, push to the results array
-  for(i = init; i < limit; i++){
-    if(sieve[i] ===1){
-      this.numbers= [i].concat(this.numbers);
-    }
-  }
-  
-  return results;
-}
 
-  //dado o ultimo numero gerado, gera o primo seguinte 
-  getPrimeNumber (num: number): number{
-    for(var i = num+2; i > 0; i= i+2){
-        if (this.numbers[i]){
-            return i;
-        }
-    }
+    // If the number is prime, push to the results array
+    for(i = init; i < limit; i++){
+      if(sieve[i]){
+        this.numbers= [i].concat(this.numbers);
+      }
+    } 
   }
-  
+
+  generatePrimes(): void{
+    var interval = setInterval(()=>{
+      if(!this.stopped && !this.paused){
+        var newSieve = this.sieveNumber + this.constant;
+        this.sieveOfAtkin(this.sieveNumber,newSieve);
+        this.sieveNumber = newSieve;
+      }
+      else{
+        clearInterval(interval);
+      }
+    },10);
+  }
+
   start (): void {
     this.started = true;
     this.paused = false;
@@ -85,36 +85,17 @@ export class MainComponent{
     this.numbers=[3,2];
     this.sieveNumber = 0;
     this.constant = 100;
-
-    var interval1 = setInterval(()=>{
-      if(!this.stopped && !this.paused){
-        var newSieve = this.sieveNumber + this.constant;
-        this.sieveOfAtkin(this.sieveNumber,newSieve);
-        this.sieveNumber = newSieve;
-      }
-      else{
-        clearInterval(interval1);
-      }
-    },50);
+    this.generatePrimes();
   }
 
   pause(): void{
     this.paused = true;
-  }
+  } 
 
   resume(): void{
     this.paused = false;
     //chama a função a cada 0,05 segundos para gerar um novo primo
-    var interval1 = setInterval(()=>{
-      if(!this.stopped && !this.paused){
-        var newSieve = this.sieveNumber + this.constant;
-        this.sieveOfAtkin(this.sieveNumber,newSieve);
-        this.sieveNumber = newSieve;
-      }
-      else{
-        clearInterval(interval1);
-      }
-    },50);
+    this.generatePrimes();
   }
 
   stop(): void{
@@ -138,6 +119,5 @@ export class MainComponent{
     return index;
   }
 }
-
 
 
